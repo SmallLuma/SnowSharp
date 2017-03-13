6dc89e6a68f8b08da529c6a509c91c92f894fe0d
hasee <hasee@GOW>
2017-03-13 22:52:34 +0800

    FileSystem

---
+++ OnWindows/LocalFileSource.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using OpenTK;
+using SnowSharp;
+namespace OnWindows
+{
+    public class LocalFileSource:FileSystem.ISource
+    {
+        public Stream OpenFile(string path)
+        {
+            try
+            {
+                return File.OpenRead(dir + path);
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+        
+
+        /// <summary>
+        /// 设置起始目录
+        /// </summary>
+        /// <param name="path">起始目录</param>
+        public void SetDir(string path)
+        {
+            dir = path + '/';
+        }
+
+        string dir;
+    }
+}
