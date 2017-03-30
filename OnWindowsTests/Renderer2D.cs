eef2baf80895fb1b4a4f8c4c86da5e394ec3bfb8
星翼 <853974536@qq.com>
2017-03-30 23:40:14 +0800

    Test Draw Call Create

--- OnWindowsTests/Renderer2D.cs
+++ OnWindowsTests/Renderer2D.cs
@@ -43,7 +43,7 @@ void main(){
             mat2Dloader.Shader = shaderLoader.LoadShader();
 
             var mat2D = mat2Dloader.LoadMateria();
-
+            var drawCall = mat2D.CreateDrawCall();
         }
     }
 }
