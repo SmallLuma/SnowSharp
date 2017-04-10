fb7510d238a7a2c1090c91e6c6a51d75b126132d
星翼 <853974536@qq.com>
2017-04-10 20:05:44 +0800

    音频设计完成

---
+++ SnowSharpOnAndroid/Sound/SoundPlayer.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK;
+
+namespace SnowSharp.Sound
+{
+
+    /// <summary>
+    /// 音频播放器
+    /// </summary>
+    public abstract class SoundPlayer
+    {
+
+        /// <summary>
+        /// 变调
+        /// </summary>
+        public abstract float Pitch
+        {
+            set;
+            get;
+        }
+
+
+        /// <summary>
+        /// 增益
+        /// </summary>
+        public abstract float Gain
+        {
+            set;
+            get;
+        }
+
+
+        /// <summary>
+        /// 播放位置
+        /// </summary>
+        public abstract Vector3 Position
+        {
+            set;
+            get;
+        }
+
+
+        /// <summary>
+        /// 是否正在循环
+        /// </summary>
+        public abstract bool Looping
+        {
+            set;
+            get;
+        }
+
+
+        /// <summary>
+        /// 已经入队的Buffer数
+        /// </summary>
+        public abstract int QueuedBuffer
+        {
+            get;
+        }
+
+
+        /// <summary>
+        /// 已经处理过的Buffer数量
+        /// </summary>
+        public abstract int QueueProcessedBuffer
+        {
+            get;
+        }
+
+        /// <summary>
+        /// 在播放队列中添加Buffer
+        /// 使用流式播放，将会取消SetBuffer的内容。
+        /// </summary>
+        /// <param name="buffer">要加入的Buffer</param>
+        public abstract void QueueBuffer(ISoundBuffer buffer);
+
+
+        /// <summary>
+        /// 从队列中弹出音频
+        /// </summary>
+        /// <param name="n">弹出数量</param>
+        public abstract void DequeueBuffer(int n);
+
+
+        /// <summary>
+        /// 清空队列
+        /// </summary>
+        public abstract void ClearBuffers();
+
+
+
+        /// <summary>
+        /// 设置要播放的Buffer
+        /// 使用静态播放，将会清空队列内容。
+        /// </summary>
+        /// <param name="buffer">Buffer</param>
+        public abstract void SetBuffer(ISoundBuffer buffer);
+
+
+        /// <summary>
+        /// 播放
+        /// </summary>
+        public abstract void Play();
+
+
+        /// <summary>
+        /// 停止
+        /// </summary>
+        public abstract void Stop();
+
+
+        /// <summary>
+        /// 暂停
+        /// </summary>
+        public abstract void Pause();
+
+
+        /// <summary>
+        /// 继续
+        /// </summary>
+        public abstract void Resume();
+    }
+}
